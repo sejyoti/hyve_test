f13d13decf527bc360b1a475beaebf3bc637fb29
Sejyoti Chakraborty <118789948+sejyoti@users.noreply.github.com>
2023-03-28 23:39:05 +0530

    Update Unit test.py

--- Unit test.py
+++ Unit test.py
@@ -1,74 +1,35 @@
+import io
+import os
 import unittest
+from decoding import decode_data
 
-from io import BytesIO
-
-from decoder import decode_pairs
-from encoder import encode_pairs
-from main import run
-
-class TestDecoder(unittest.TestCase):
-
-    def test_decode_pairs(self):
-        # Test decoding a simple pair
-        encoded_data = [(0, 97)]
-        decoded_data = decode_pairs(encoded_data)
-        self.assertEqual(decoded_data, b'a')
-
-        # Test decoding a pair that references a previous position
-        encoded_data = [(0, 97), (1, 1)]
-        decoded_data = decode_pairs(encoded_data)
-        self.assertEqual(decoded_data, b'aa')
-
-        # Test decoding a pair that is too long
-        encoded_data = [(0, 97), (2, 2)]
-        decoded_data = decode_pairs(encoded_data)
-        self.assertEqual(decoded_data, b'\xe2\x9c\xb8\xe2\x9d\x8b')
-
-    def test_encode_pairs(self):
-        # Test encoding a simple byte
-        decoded_data = b'a'
-        encoded_data = encode_pairs(decoded_data)
-        self.assertEqual(encoded_data, [(0, 97)])
-
-        # Test encoding a sequence with repeated bytes
-        decoded_data = b'aaaaaa'
-        encoded_data = encode_pairs(decoded_data)
-        self.assertEqual(encoded_data, [(0, 97), (5, 1)])
-
-        # Test encoding a sequence with a long repeated subsequence
-        decoded_data = b'abcdefabcdefabcdef'
-        encoded_data = encode_pairs(decoded_data)
-        self.assertEqual(encoded_data, [(0, 97), (1, 1), (1, 1), (1, 1), (6, 6)])
-
-class TestMain(unittest.TestCase):
-
-    def test_run(self):
-        # Test decoding and re-encoding a simple byte
-        input_data = BytesIO(b'\x00a')
-        output_data = BytesIO()
-        error_data = BytesIO()
-        run(input_data, output_data, error_data)
-        self.assertEqual(output_data.getvalue(), b'a')
-        self.assertEqual(error_data.getvalue(), b'\x00a')
-
-        # Test decoding and re-encoding a sequence with repeated bytes
-        input_data = BytesIO(b'\x00a\x05\x01')
-        output_data = BytesIO()
-        error_data = BytesIO()
-        run(input_data, output_data, error_data)
-        self.assertEqual(output_data.getvalue(), b'aaaaa')
-        self.assertEqual(error_data.getvalue(), b'\x00a\x05\x01')
-
-        # Test decoding and re-encoding a sequence with a long repeated subsequence
-        input_data = BytesIO(b'\x00a\x01\x01\x01\x06\x06')
-        output_data = BytesIO()
-        error_data = BytesIO()
-        run(input_data, output_data, error_data)
-        self.assertEqual(output_data.getvalue(), b'abcdefabcdefabcdef')
-        self.assertEqual(error_data.getvalue(), b'\x00a\x01\x01\x01\x06\x06')
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-These tests cover the basic functionality of the encoder, decoder, and main functions. However, additional tests could be written to test edge cases or more complex data.
+class TestDecoding(unittest.TestCase):
+    
+    def test_decode_data(self):
+        # Example input from the prompt
+        input_data = b'\x00\x61\x01\x01\x00\x62\x03\x02\x03\x03'
+        expected_output = b'\x61\x61\x62\x61\x61\x62\x61\x61\x62\x61\x61\x62\x61\x61'
+        
+        # Redirect stdout to a buffer
+        stdout_backup = sys.stdout
+        sys.stdout = io.StringIO()
+        
+        # Call decode_data on the input data
+        decode_data(input_data)
+        
+        # Get the decoded data from stdout buffer
+        decoded_output = sys.stdout.getvalue().encode()
+        
+        # Check if decoded_output matches expected_output
+        self.assertEqual(decoded_output, expected_output)
+        
+        # Get the re-encoded data from stderr buffer
+        re_encoded_data = sys.stderr.getvalue().encode()
+        
+        # Check if the re-encoded data matches the input data
+        self.assertEqual(re_encoded_data, input_data)
+        
+        # Clean up stdout and stderr
+        sys.stdout = stdout_backup
+        sys.stderr.seek(0)
+        sys.stderr.truncate(0)
